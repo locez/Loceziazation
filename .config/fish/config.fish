05f157dbdcea08bf0b1714f3a3cbace5659d1499
Locez <locez@locez.com>
2026-03-19 23:14:07 +0800

    feat: update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
     starship init fish | source
-    zoxide init fish | source 
+    zoxide init --cmd cd fish | source
+    atuin init fish | source
     # Commands to run in interactive sessions can go here
 end
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+set -gx MAMBA_EXE "/usr/bin/mamba"
+set -gx MAMBA_ROOT_PREFIX "/home/locez/.local/share/mamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
+~/.local/bin/mise activate fish | source
